797ae0387e85ff4b23f42a728798387494b2e807
Lucas Lacava <lucaslacava@gmail.com>
2025-05-27 21:41:08 -0300

    chore: update fonts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,20 +1,9 @@
 import localFont from "next/font/local";
 
-export const ttNormsSerif = localFont({
-  src: [
-    {
-      path: "../fonts/TT Norms Pro Serif Trial Normal.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../fonts/TT Norms Pro Serif Trial Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-  ],
-  variable: "--font-tt-norms-serif",
+export const ttNorms = localFont({
+  src: "../fonts/TTNorms-Medium.otf",
+  variable: "--font-tt-norms",
   display: "swap",
   preload: true,
-  fallback: ["serif"],
+  fallback: ["sans-serif"],
 });
