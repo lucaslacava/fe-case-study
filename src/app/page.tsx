fc8312baf25549aa29312637db0ffd1908aa519b
Lucas Lacava <lucaslacava@gmail.com>
2025-05-27 21:42:37 -0300

    chore: font update and improve structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,20 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useQuiz } from "@/context/QuizContext";
 import { QuizProvider } from "@/context/QuizContext";
 import { Quiz } from "./pages/Quiz/Quiz";
+import { LandingPage } from "./pages/LandingPage/LandingPage";
 
-function LandingPage() {
-  const { setShowQuiz, showQuiz } = useQuiz();
+function Content() {
+  const { showQuiz } = useQuiz();
 
-  const handleStartQuiz = () => {
-    setShowQuiz(true);
-  };
-
-  if (showQuiz) {
-    return <Quiz />;
-  }
-
-  return (
-    <div className="min-h-screen w-full bg-white text-black flex flex-col items-center justify-center p-8">
-      <div className="max-w-2xl w-full space-y-8 text-center">
-        {/* Landing page content */}
-        <Button onClick={handleStartQuiz} className="px-8 py-6 text-lg">
-          Take the Quiz
-        </Button>
-      </div>
-    </div>
-  );
+  return showQuiz ? <Quiz /> : <LandingPage />;
 }
 
 export default function Home() {
   return (
     <QuizProvider>
-      <LandingPage />
+      <Content />
     </QuizProvider>
   );
 }
